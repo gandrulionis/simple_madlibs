e997d0a47a403b021064fcc36b48a72219c7f948
gandrulionis <gandrulionis1@gmail.com>
2023-03-04 23:16:40 +0200

    Mad-Libs simple project

---
+++ main.py
@@ -0,0 +1,16 @@
+
+adjective = input("Adjective: ")
+
+verb1 = input("Verb 1: ")
+
+verb2 = input("Verb 2: ")
+
+famous_person = input("Famous person: ")
+
+
+print(f"Programming is so {adjective}, I really love to {verb1}. \nStay hydrated and {verb2}, like {famous_person}.")
+
+
+
+
+
